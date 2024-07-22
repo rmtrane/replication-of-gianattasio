d3514194458cf4d7e593ce90cff4214c74e2de24
Ralph Trane <rmtrane@gmail.com>
2024-07-22 18:47:55 -0500

    Forgot to include bash

--- scripts/bash/run_HRS_sas_file.sh
+++ scripts/bash/run_HRS_sas_file.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-while getopts ":o:" opt; do
-  case $opt in
-    o) out="$OPTARG";;
-  esac
-
-  case $OPTARG in
-    -*) echo "Option $opt needs a valid argument"
-    exit 1
-    ;;
-  esac
+find data/HRS-unzips/h*/new_sas -iname "*.sas" | while read f
+do
+    sas $f
 done
-
-source="${out:0:3}"
-
-sasfilename="${out%.*}"
-
-sas data/HRS-unzips/$source/new_sas/$sasfilename.sas
