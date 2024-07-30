80a11e81b411d0eadc343fc8f1a3003f2caaa24c
Ralph Trane <rmtrane@gmail.com>
2024-07-29 19:04:12 -0500

    Updated update_sas_file.sh to deal with differing LRECL and weird space patterns

--- scripts/bash/update_sas_file.sh
+++ scripts/bash/update_sas_file.sh
@@ -23,14 +23,6 @@ infolder=`dirname $(readlink -f $file)`
 dafolder=`dirname $infolder`/da
 outfolder=${infolder%/data/*}/data/SAS
 
-# If ADAMS file, add /ADAMS to outfolder, otherwise add /HRS
-case "$file" in
-    *adams*)
-	outfolder=$outfolder/ADAMS;;
-    *h*/sas/*)
-	outfolder=$outfolder/HRS;;
-esac
-
 # Create folder for new SAS files
 mkdir -p `dirname $infolder`/new_sas
 
@@ -42,6 +34,12 @@ filename=${filename%.*}
 # Next, create inputfile
 inputfile=$dafolder/$filename.da
 
-LC_ALL=C sed "s|LIBNAME .*|LIBNAME ADAMS1 \"${outfolder}\";|" $file |
-    LC_ALL=C sed "s|LIBNAME .*|LIBNAME EXTRACT \"${outfolder}\";|" $file |
-    LC_ALL=C sed "s|INFILE .*|INFILE \"${inputfile}\" LRECL=1268;|" > `dirname $infolder`/new_sas/$filename.sas
+#echo $file
+
+sed -E "s|^LIBNAME (.*) '.*'|LIBNAME \1 '${outfolder}'|I" $file | 
+    sed -E "s|^INFILE .* LRECL[[:space:]]*=[[:space:]]*([0-9]+)|INFILE '${inputfile}' LRECL=\1|I" > `dirname $infolder`/new_sas/$filename.sas
+
+
+#LC_ALL=C sed "s|^LIBNAME .*|LIBNAME ${libname} \"${outfolder}\";|" $file |
+#    LC_ALL=C sed "s|^libname .*|LIBNAME ${libname} \"${outfolder}\";|" |
+#    LC_ALL=C sed "s|INFILE .*|INFILE \"${inputfile}\" LRECL=1268;|" > `dirname $infolder`/new_sas/$filename.sas
